8fdd4570ceeb7a01c5c621c4abef2826945f5a24
Frayu <galiano@outlook.com>
2023-07-13 17:54:44 -0500

    Corregir Routes

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/main" element={<Filter />} />
-        <Route path="*" element={<h1>404 page not found</h1>} />
+        <Route path="/Clone-Funval-Isaias/" element={<App />} />
+        <Route path="/Clone-Funval-Isaias/main" element={<Filter />} />
+        <Route path="/Clone-Funval-Isaias/*" element={<h1>404 page not found</h1>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
